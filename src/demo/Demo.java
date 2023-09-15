82021319546afc88806271a99811a14033d05559
sarath <sarathezil@gmail.com>
2023-09-15 08:16:14 +0530

    new feature

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -3,11 +3,7 @@ package demo;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println("hello world");
+        System.out.println(10%3+5/2); //2 or 3
+
     }
 }
